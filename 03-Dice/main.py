e51b8c38db1a3d1d1a7dd6307333d00bf08f7043
Obi815 <80441506+Obi815@users.noreply.github.com>
2024-09-23 20:32:30 -0700

    Fixed Bugs

--- 03-Dice/main.py
+++ 03-Dice/main.py
@@ -1,6 +1,13 @@
 import random
 
+
 def roll_dice(amount: int = 2) -> list[int]:
+    """
+    Rolls some dice and returns the results as a list.
+
+    :param amount: The amount of dice to roll.
+    :return: A list of dice rolls.
+    """
     if amount <= 0:
         raise ValueError
 
@@ -12,20 +19,21 @@ def roll_dice(amount: int = 2) -> list[int]:
     return rolls
 
 
-
 def main():
-   while True:
-       try:
-           user_input = str = input('How many dice would you like to roll?' )
+    while True:
+        try:
+            user_input: str = input('How many dice would you like to roll? ')
 
-           if user_input.lower() == 'exit':
-               print('Thanks for playing!')
-               break
+            # To exit the game
+            if user_input.lower() == 'exit':
+                print('Thanks for playing!')
+                break
+
+            # Try to parse the user_input to int
+            print(*roll_dice(int(user_input)), sep=', ')
+        except ValueError:
+            print('(Please enter a valid number)')
 
 
-          print(roll_dice(int(user_input)))
-       except ValueError:
-       print('Please enter a valid number')
-
-    if __name__ = '__main__':
-        main()
+if __name__ == '__main__':
+    main()
